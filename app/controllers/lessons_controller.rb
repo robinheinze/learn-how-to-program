481fcc768eca8643edb1176d817e850f244c65df
maigen skjelstad & Robin Miller <student@epicodus.com>
2014-04-02 12:12:00 -0700

    adds complete CRUD functionality and beautiful CSS

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -15,5 +15,33 @@ class LessonsController < ApplicationController
     render('lessons/new.html.erb')
   end
 
+  def create
+    @lesson = Lesson.new(params[:lessons])
+    if @lesson.save
+      redirect_to("/lessons/#{@lesson.id}")
+    else
+      render('lessons/new.html.erb')
+    end
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+    render('lessons/edit.html.erb')
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(params[:lessons])
+      redirect_to("/lessons/#{@lesson.id}")
+    else
+      render('lessons/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    render("lessons/destroy.html.erb")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 LearnHowToProgram::Application.routes.draw do
   match('lessons', {:via => :get, :to => 'lessons#index'})
   match('lessons/new', {:via => :get, :to => 'lessons#new'})
+  match('lessons', {:via => :post, :to => 'lessons#create'})
+  match('lessons/:id/edit', {:via => :get, :to => 'lessons#edit'})
   match('lessons/:id', {:via => :get, :to => 'lessons#show'})
-
+  match('lessons/:id', {:via => [:patch, :put], :to => 'lessons#update'})
+  match('lessons/:id', {:via => :delete, :to => 'lessons#destroy'})
 end
